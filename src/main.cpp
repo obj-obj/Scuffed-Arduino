26df224f1295125090ffac27659c8ad34a8b3fec
[object Object] <object-object@outlook.com>
2022-09-23 17:36:13 -0700

    feat: funny effect

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,39 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED 9
-#define POWER 8
-#define GROUND 10
+#define PIXELS 144
 
-Adafruit_NeoPixel strip(133, LED, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(PIXELS, LED, NEO_GRB + NEO_KHZ800);
 
 void setup() {
+	pinMode(LED, OUTPUT);
+
+	Serial.begin(9600);
 	strip.begin();
 }
 
+const char saturation = 255;
+const char rainbow_brightness = 128;
+const char speed = 10;
+
+uint32_t meteor_color = strip.Color(255, 0, 0);
+const char meteor_length = 10;
+const short meteor_constant = 65535 / (PIXELS + meteor_length);
+
+unsigned short i = 0;
+
 void loop() {
-	strip.fill(strip.Color(255, 0, 0));
+	// Rainbow
+	strip.rainbow(i, 1, saturation, rainbow_brightness);
+
+	// Meteor
+	short local_constant = i / meteor_constant;
+	for (int j = local_constant; j > local_constant - meteor_length; j--) {
+		strip.setPixelColor(j, meteor_color);
+	}
+
 	strip.show();
-	delay(100);
-	strip.fill(strip.Color(0, 255, 0));
-	strip.show();
-	delay(100);
-	strip.fill(strip.Color(0, 0, 255));
-	delay(100);
-	strip.show();
+	delay(speed);
+
+	i += 256;
 }
