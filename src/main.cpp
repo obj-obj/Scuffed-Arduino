6b41d2a019e017bdeffd84a1600cb5a592cc39f4
[object Object] <object-object@outlook.com>
2022-09-26 17:01:06 -0700

    Serial input

--- src/main.cpp
+++ src/main.cpp
@@ -15,28 +15,27 @@ void setup() {
 
 // CONFIG //
 const unsigned char rainbow_saturation = 255;
-const unsigned char rainbow_brightness = 100;
-const double rainbow_speed = 10;
+const unsigned char rainbow_brightness = 255;
+double rainbow_speed = 0.02;
 
-uint32_t meteor_color = strip.Color(50, 0, 0);
 const unsigned char meteor_length = 10;
 const unsigned char meteor_pixels = PIXELS + meteor_length;
-const double meteor_speed = 0.1;
+double meteor_speed = 0.0001;
 
 // RUNTIME //
 unsigned long time = 0;
 unsigned long previous_time = 0;
 
 unsigned short ri = 0;
-short mi = 0;
+double mi = 0;
 
 void loop() {
 	time = micros();
 	const double delta_time = (time - previous_time);
 	previous_time = time;
 
-	ri += (rainbow_speed / 1000) * delta_time;
-	mi += (meteor_speed / 1000) * delta_time;
+	ri += rainbow_speed * delta_time;
+	mi += meteor_speed * delta_time;
 	while (mi >= meteor_pixels) {
 		mi -= meteor_pixels + meteor_length;
 	}
@@ -46,8 +45,20 @@ void loop() {
 
 	// Meteor
 	for (int j = mi; j > mi - meteor_length; j--) {
-		strip.setPixelColor(j, meteor_color);
+		strip.setPixelColor(j, strip.Color(255, 0, 0));
 	}
 
 	strip.show();
+
+	// Serial parsing
+	if (Serial.available() > 0) {
+		String inp = Serial.readString();
+
+		if (inp.startsWith("rs")) {
+			rainbow_speed = ((double)atoi(inp.substring(3).c_str())) / 1000;
+		}
+		else if (inp.startsWith("ms")) {
+			meteor_speed = ((double)atoi(inp.substring(3).c_str())) / 100000;
+		}
+	}
 }
